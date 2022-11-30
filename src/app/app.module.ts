426c9c95fc01d25f883cc1608423f937403b2e3b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-11-30 15:20:46 +0530

    create a new component odd using the CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {GameControlComponent} from './game-control/game-control.component';
+import {OddComponent} from './odd/odd.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameControlComponent
+    GameControlComponent,
+    OddComponent
   ],
   imports: [
     BrowserModule
