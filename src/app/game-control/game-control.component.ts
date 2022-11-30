1276f81a981a94c74e51c9f3f05ea742de0c88a4
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-11-30 15:35:45 +0530

    initialize a field intervalFired with object of class EventEmitter<number>

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -7,6 +7,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class GameControlComponent implements OnInit {
 
+  intervalFired = new EventEmitter<number>();
   interval;
 
   constructor() {
